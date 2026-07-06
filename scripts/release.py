0f1b6f33ba5dcdb2e40f8272d68cc9057bff11d3
teknium1 <127238744+teknium1@users.noreply.github.com>
2026-07-06 12:11:33 -0700

    chore: add tanmayxchoudhary to AUTHOR_MAP

--- scripts/release.py
+++ scripts/release.py
@@ -284,6 +284,7 @@ AUTHOR_MAP = {
     "290859878+synapsesx@users.noreply.github.com": "synapsesx",
     "157689911+itsflownium@users.noreply.github.com": "itsflownium",
     "dirtyren@users.noreply.github.com": "dirtyren",
+    "hello@tanmaychoudhary.com": "tanmayxchoudhary",
     "285906080+AIalliAI@users.noreply.github.com": "AIalliAI",
     "waseemshahwan@users.noreply.github.com": "waseemshahwan",
     "hellno@users.noreply.github.com": "hellno",
